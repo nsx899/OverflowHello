a6545b5deabd3d13b2ae7192db1d63b8fb1f12b9
János Kiss <kiss.janos753@gmail.com>
2024-03-24 19:23:05 +0100

    Implement OverflowView

---
+++ OverflowHello/ReferencesView.swift
@@ -0,0 +1,18 @@
+//
+//  ReferencesView.swift
+//  OverflowHello
+//
+//  Created by Kiss János on 24/03/2024.
+//
+
+import SwiftUI
+
+struct ReferencesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReferencesView()
+}
